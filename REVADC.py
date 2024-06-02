2855193d5182491e88349c2bd31baeb37ef46b0e
Iris <87090296+Iris-TheRainbow@users.noreply.github.com>
2024-06-01 19:35:24 -0500

    pydoc for adc

--- REVADC.py
+++ REVADC.py
@@ -1,4 +1,6 @@
 from . import REVMessage as REVMsg
+
+##Some constants
 ADC_INPUT_CHANNEL_0 = 0
 ADC_INPUT_CHANNEL_1 = 1
 ADC_INPUT_CHANNEL_2 = 2
@@ -17,24 +19,30 @@ CONTROLLER_TEMPERATURE = 14
 
 ##Note: appears to just be part of the current sensing for motors? (ADC = analog digital converter)
 class ADCPin:
+    """Driver for ADC (current and voltage sensors mostly)"""
     def __init__(self, commObj, channel, destinationModule):
         self.commObj = commObj
         self.channel = channel
         self.destinationModule = destinationModule
 
     def setDestination(self, destinationModule):
+        """Set ADC destination module"""
         self.destinationModule = destinationModule
 
     def getDestination(self):
+        """Get set destination module"""
         return self.destinationModule
 
     def setChannel(self, channel):
+        """Set the ADC Channel"""
         self.channel = channel
 
     def getChannel(self):
+        """Get ADC Channel"""
         return self.channel
 
     def getADC(self, rawMode):
+        """Get all ADC (?)"""
         getADC = REVMsg.GetADC()
         getADC.payload.adcChannel = self.channel
         getADC.payload.rawMode = rawMode
