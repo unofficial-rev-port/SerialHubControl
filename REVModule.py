4e829ee8f4e8bd94506f53971769249c5bf9343e
Iris <87090296+Iris-TheRainbow@users.noreply.github.com>
2024-06-01 21:24:23 -0500

    pydoc for revmodule

--- REVModule.py
+++ REVModule.py
@@ -2,6 +2,7 @@ from . import REVMotor, REVServo, REVADC, REVDIO, REVI2C
 
 ##Note: Modules are hubs (lynx modules)
 class Module:
+    """Lynx Module device"""
     def __init__(self, commObj, address, parent):
         self.commObj = commObj
         self.address = address
@@ -13,6 +14,7 @@ class Module:
         self.dioPins = []
 
     def init_periphs(self):
+        """Initalizes all devices (Motors, Servos, Digital I/O, and ADC"""
         for i in range(0, 4):
             self.motors.append(REVMotor.Motor(self.commObj, i, self.address))
             self.motors[-1].setMode(0, 1)
@@ -30,7 +32,7 @@ class Module:
             self.adcPins.append(REVADC.ADCPin(self.commObj, l, self.address))
 
     def killSwitch(self):
-        #disable everything for safe stop
+        """Disable everything for safe stop"""
         for i in range(0, 4):
             self.motors[i].disable()
 
@@ -38,24 +40,31 @@ class Module:
             self.servos[k].disable()
 
     def getParentStatus(self):
+        """Get status of parent module (?)"""
         return self.parent
 
     def getAddress(self):
+        """Get address of lynx module, functionally identical to getModuleAddress()"""
         return self.address
 
     def getStatus(self):
+        """Get lynx module """
         return self.commObj.getModuleStatus(self.address)
 
     def getModuleAddress(self):
+        """Get address of lynx module, functionally identical to getAddress()"""
         return self.address
 
     def sendKA(self):
+        """Keep alive for lynx module"""
         return self.commObj.keepAlive(self.address)
 
     def sendFailSafe(self):
+        """Failsafe for lynx module"""
         self.commObj.failSafe(self.address)
 
     def setAddress(self, newAddress):
+        """Set the address of the lynx module and update all devices addresses"""
         self.commObj.setNewModuleAddress(self.address, newAddress)
         self.address = newAddress
         for motor in self.motors:
@@ -74,16 +83,21 @@ class Module:
             dioPin.setDestination(newAddress)
 
     def getInterface(self, interface):
+        """Get the interface of the lynx module, direct serial or RS485 (?)"""
         return self.commObj.queryInterface(self.address, interface)
 
     def setLEDColor(self, red, green, blue):
+        """Set the color of the LED on the lynx module"""
         self.commObj.setModuleLEDColor(self.address, red, green, blue)
 
     def getLEDColor(self):
+        """Get the current RGB value of the lynx module"""
         return self.commObj.getModuleLEDColor(self.address)
 
     def setLEDPattern(self, pattern):
-        """ Example:
+        """ 
+      Set a LED patern for the LED of the lynx module
+      Example:
       from REVmessages import LEDPattern
 
       hub = REVModules()
@@ -96,30 +110,39 @@ class Module:
         return self.commObj.setModuleLEDPattern(self.address, pattern)
 
     def setLogLevel(self, group, verbosity):
+        """Set the logging level"""
         self.commObj.debugLogLevel(self.address, group, verbosity)
 
     def getBulkData(self):
+        """Get bulk input data, analagous to FTC SDK bulkread"""
         return self.commObj.getBulkInputData(self.address)
 
     def enableCharging(self):
+        """Enable Android Phone charging"""
         self.commObj.phoneChargeControl(self.address, 1)
 
     def disableCharging(self):
+        """Disable Android Phone charging"""
         self.commObj.phoneChargeControl(self.address, 0)
 
     def chargingEnabled(self):
+        """Check android phone charging state"""
         return self.commObj.phoneChargeQuery(self.address)
 
     def debugOutput(self, length, hint):
+        """Debug the output"""
         self.commObj.injectDataLogHint(self.address, length, hint)
 
     def setAllDIO(self, values):
+        """Set all Digital I/O"""
         REVDIO.setAllDIOOutputs(self.address, values)
 
     def getAllDIO(self):
+        """Read all Digital I/O"""
         return REVDIO.getAllDIOInputs(self.address)
 
     def getVersionString(self):
+        """Read the firmware version"""
         versionRaw = '' + self.commObj.readVersionString(self.address)
         versionStr = ''
         for i in range(0, int(len(versionRaw) / 2)):
@@ -128,7 +151,10 @@ class Module:
         return versionStr
 
     def setIMUBlockReadConfig(self, startRegister, numberOfBytes, readInterval_ms):
+        """Set the Configuration of IMU block read"""
         REVI2C.imuBlockReadConfig(self.address, startRegister, numberOfBytes, readInterval_ms)
 
     def getIMUBlockReadConfig(self):
+        """Get the configuration of IMU block read"""
         return REVI2C.imuBlockReadQuery(self.address)
+    
